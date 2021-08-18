a52b7fabe177619bb425cd3d2b2512ce3963dcf5
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-18 18:50:54 -0400

    testing a push

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code></code>
+          HELLO
         </p>
         <a
           className="App-link"
