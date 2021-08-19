96545e475beadc815c140a601ec2247dc2f0d7c0
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-19 07:26:17 -0400

    testing this commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HELLOS
+          THIS IS A TEST
         </p>
         <a
           className="App-link"
