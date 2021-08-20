9f8e3a8968c31a17f05780757c9dabcf60317fb4
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-20 16:13:51 -0400

    basics on about me page and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div>
-            TESTY FOOTER
+        <div style={{bottom:"0", height: "6em"}}>
+            <ul style={{listStyleType:"none", display: "flex", justifyContent: "row"}}>
+                <li><a style={{textDecoration: "none", display: "block"}} href="https://www.linkedin.com/in/meghan-murphy-111445179/">LinkedIn</a></li>
+                <li><a style={{textDecoration: "none", display: "block"}} href="https://github.com/MMURPHY222">GitHub</a></li>
+                <li><a style={{textDecoration: "none", display: "block"}} href="https://twitter.com/BarackObama?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor">Twitter</a></li>
+            </ul>
         </div>
     )
 }
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,21 +1,30 @@
 import React from 'react';
-
+import ProfilePic from '../../img/profile picture.JPG'
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+      <h1>MM</h1>
+      <div style={{display: "flex", flexWrap: "wrap", flexDirection: "row"}}>
+      <p style={{width:"50em"}}>
+      Hello, I am interested in a full stack developer position! My name is Meghan Murphy, I have a 
+      degree in Anthropology from Emory University, and a Certificate in Full Stack Web Development 
+      from Georgia Tech. I am a former college athlete, outdoors lover, and dog fanatic. College was a 
+      balancing act between a full course load, many hours of softball practice each week, and working 
+      at the Student Athletic Center, oftentimes starting my days at 5am and not getting home until 9 or 
+      10 at night. My years at Emory shaped my strong work ethic, and my experiences as a competitive 
+      athlete have given me the ability to communicate respectfully and effectively with a team. I jump 
+      into new projects with curiosity and enthusiasm, unafraid to take on a leadership role within a 
+      group, but also content to step back and let others take the reigns. I love working with people 
+      from diverse backgrounds and seeing different cultures and worldviews come together to make a 
+      unique, multifaceted product. During my bootcamp I have applied aspects of UX and agile development 
+      in a recent project where I collaborated in a team of five to develop a multi-page MERN app that 
+      allows users to post fan-fiction stories and other users to comment on their posts. I am excited to 
+      bring my expertise in HTML, CSS, and JavaScript to a collaborative team that promotes social change 
+      through beautiful and responsive design.. I also hope to lend my future tech skills to non-profits, 
+      specifically animal shelters where I do frequent volunteer work.  
       </p>
+      <img style={{height:"20em", width:"20em", marginLeft: "2em"}}src={ProfilePic} alt="..."></img>
+      </div>
     </div>
   );
 }
