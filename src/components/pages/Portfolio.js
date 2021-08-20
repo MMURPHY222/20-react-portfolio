39669facad66e92e4f17fcbcd8e2fd56d780f195
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-20 11:01:23 -0400

    updated card stylings

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,58 +4,58 @@ export default function Portfolio() {
   return (
     <div>
       <h1>Portfolio Page</h1>
-      <div class="card" style={{width: "18rem",}}>
+      <div class="card" style={{width: "18rem", padding: ".5em",}}>
           <img src="..." class="card-img-top" alt="..."/>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Market Finder</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#thing" class="btn btn-primary">GitHub</a>
-            <a href="#thing" class="btn btn-primary">Live Link</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>GitHub</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>Live Link</a>
           </div>
       </div>
-      <div class="card" style={{width: "18rem",}}>
+      <div class="card" style={{width: "18rem", padding: ".5em",}}>
           <img src="..." class="card-img-top" alt="..."/>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">FanFiction</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#thing" class="btn btn-primary">GitHub</a>
-            <a href="#thing" class="btn btn-primary">Live Link</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>GitHub</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>Live Link</a>
           </div>
       </div>
-      <div class="card" style={{width: "18rem",}}>
+      <div class="card" style={{width: "18rem", padding: ".5em",}}>
           <img src="..." class="card-img-top" alt="..."/>   
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Montana Quiz</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#thing" class="btn btn-primary">GitHub</a>
-            <a href="#thing" class="btn btn-primary">Live Link</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>GitHub</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>Live Link</a>
           </div>
       </div>
-      <div class="card" style={{width: "18rem",}}>
+      <div class="card" style={{width: "18rem", padding: ".5em",}}>
           <img src="..." class="card-img-top" alt="..."/>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Weather Dashboard</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#thing" class="btn btn-primary">GitHub</a>
-            <a href="#thing" class="btn btn-primary">Live Link</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>GitHub</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>Live Link</a>
           </div>
       </div>
-      <div class="card" style={{width: "18rem",}}>
+      <div class="card" style={{width: "18rem", padding: ".5em",}}>
           <img src="..." class="card-img-top" alt="..."/>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Planner</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#thing" class="btn btn-primary">GitHub</a>
-            <a href="#thing" class="btn btn-primary">Live Link</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>GitHub</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>Live Link</a>
           </div>
       </div>
-      <div class="card" style={{width: "18rem",}}>
+      <div class="card" style={{width: "18rem", padding: ".5em",}}>
           <img src="..." class="card-img-top" alt="..."/>
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">Fitness Tracker</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#thing" class="btn btn-primary">GitHub</a>
-            <a href="#thing" class="btn btn-primary">Live Link</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>GitHub</a>
+            <a href="#thing" class="btn btn-primary" style={{padding: ".5em",}}>Live Link</a>
           </div>
       </div>
     </div>
