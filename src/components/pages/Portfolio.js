7eaa2791bb354d15ff889820afda08e4ee23988f
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-20 22:03:53 -0400

    added descriptions to projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -12,13 +12,12 @@ export default function Portfolio() {
     <div style={{position: "relative", minHeight: "100vh", background: "#507982"}}>
       <h1>Check out my work</h1>
       <div style={{display: "flex", flexWrap: "wrap", flexDirection: "row"}}>
-      <Project name={"Market Finder"} img={MarketFinder} github={"https://github.com/MMURPHY222/Project-1"} livelink={"https://mmurphy222.github.io/Project-1/"} description={"DESCRIPTION"}/>
-      <Project name={"Fan Fiction"} img={FanFic} github={"https://github.com/MMURPHY222/Project-2-FanFic"} livelink={"https://mysterious-lowlands-06062.herokuapp.com/"} description={"DESCRIPTION"}/>
-      <Project name={"Montana Quiz"} img={Montana} github={"https://github.com/MMURPHY222/logans-quiz"} livelink={"https://mmurphy222.github.io/logans-quiz/"} description={"DESCRIPTION"}/>
-
-      <Project name={"Weather Dashboard"} img={Weather} github={"https://github.com/MMURPHY222/06-weather-dashboard"} livelink={"https://mmurphy222.github.io/06-weather-dashboard/"} description={"DESCRIPTION"}/>
-      <Project name={"Planner"} img={Planner} github={"https://github.com/MMURPHY222/05-day-planner"} livelink={"https://mmurphy222.github.io/05-day-planner/"} description={"DESCRIPTION"}/>
-      <Project name={"Fitness Tracker"} img={Fitness} github={"https://github.com/MMURPHY222/18-fitness-tracker"} livelink={"https://limitless-anchorage-11451.herokuapp.com/?id=611713295aee6800166e74cb"} description={"DESCRIPTION"}/>
+      <Project name={"Market Finder"} img={MarketFinder} github={"https://github.com/MMURPHY222/Project-1"} livelink={"https://mmurphy222.github.io/Project-1/"} description={"Technology: Javascript, HTML, CSS, APIs. App for users to enter their zip code, find local organic farmer's markets near them."}/>
+      <Project name={"Fan Fiction"} img={FanFic} github={"https://github.com/MMURPHY222/Project-2-FanFic"} livelink={"https://mysterious-lowlands-06062.herokuapp.com/"} description={"Technology: Javascript, node.js, express, MySQL. App for users to write fan fiction, read fan fiction, and comment on other user's posts."}/>
+      <Project name={"Montana Quiz"} img={Montana} github={"https://github.com/MMURPHY222/logans-quiz"} livelink={"https://mmurphy222.github.io/logans-quiz/"} description={"Technology: Javascript, HTML, CSS. Quiz app based on Montana trivia questions. Users have 100 seconds to complete it and lose time for incorrect answers."}/>
+      <Project name={"Weather Dashboard"} img={Weather} github={"https://github.com/MMURPHY222/06-weather-dashboard"} livelink={"https://mmurphy222.github.io/06-weather-dashboard/"} description={"Technology: Javascript, HTML, CSS, APIs. App for a weather search based on location. Gives 5 day forecast and dynamic UVI index."}/>
+      <Project name={"Planner"} img={Planner} github={"https://github.com/MMURPHY222/05-day-planner"} livelink={"https://mmurphy222.github.io/05-day-planner/"} description={"Technology: Javascript, HTML, CSS. App for planning activities throughout a day. Dynamic colored hours based on time in user's current location. Local storage used for keeping entries through page refresh."}/>
+      <Project name={"Fitness Tracker"} img={Fitness} github={"https://github.com/MMURPHY222/18-fitness-tracker"} livelink={"https://limitless-anchorage-11451.herokuapp.com/?id=611713295aee6800166e74cb"} description={"Technology: Javascript, MongoDB, HTML, CSS. App used to track workouts using a mongo database. Allows for adding to previous workouts, creating new workouts and graphing them."}/>
       </div>
     </div>
   );
