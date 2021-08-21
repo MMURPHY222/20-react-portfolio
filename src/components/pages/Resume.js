6bfbf8cf5aa26b571870faedf93f05c262e2d9bb
Meghan Murphy <meghanmurphy.212@gmail.com>
2021-08-20 22:12:00 -0400

    added skills to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,7 +4,8 @@ import ResumePdf from '../../img/Developer Resume-converted.pdf'
 export default function Resume() {
   return (
     <div style={{height: "100vh", background: "#507982", justifyContent: "center", alignItems: "center"}}>
-      <h1> <a style={{textDecoration: "none", color: "black", marginTop: "10em", marginLeft: "33%"}} href={ResumePdf}>Download My Resume</a></h1>
+      <h1> <a style={{textDecoration: "none", color: "black", paddingTop: "10em", marginLeft: "33%"}} href={ResumePdf}>Download My Resume</a></h1>
+      <p style={{marginLeft:"25%"}}>Skills: React.js, Javascript, node.js, express.js, MySQL, MongoDB, HTML, CSS</p>
     </div>
   );
 }
